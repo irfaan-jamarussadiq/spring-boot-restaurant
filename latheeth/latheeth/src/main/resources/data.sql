f81ebf2e76d78d7fb60666ea636dd5dad13fcdd0
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2024-04-27 17:42:05 -0400

    Update database table configuration

--- latheeth/latheeth/src/main/resources/data.sql
+++ latheeth/latheeth/src/main/resources/data.sql
@@ -1,4 +1,11 @@
-insert into menu_item(name, image, description) values('Pakora', 'pakora.jpg', 'Fried indian snack with chutney');
-insert into menu_item(name, image, description) values('Beef Cutlet', 'cutlet.jpg', 'Fried breadcrumbs snack with beef inner filling');
-insert into menu_item(name, image, description) values('Shami Kabab', 'shami-kabab.jpg', 'Beef and lentil patties with daal and spices');
-insert into menu_item(name, image, description) values('Samosa', 'samosa.jpg', 'Indian triangular shaped snack stuffed with potatoes and peas');
+-- Menu
+insert into item(name, description, image, unit_price) values('Pakora', 'Indian fritters made with potatoes and onion, served with chutney', 'pakora.jpg', 4.99);
+insert into item(name, description, image, unit_price) values('Beef Cutlet', 'Fried fritters covered in breadcrumbs with beef inner filling', 'cutlet.jpg', 8.99);
+insert into item(name, description, image, unit_price) values('Shami Kabab', 'Beef and lentil patties with daal and spices', 'shami-kabab.jpg', 8.99);
+insert into item(name, description, image, unit_price) values('Samosa', 'Indian triangular shaped snack stuffed with potatoes and peas', 'samosa.jpg', 3.99);
+
+-- Order
+insert into order_item(item_id, count) values(1, 5);
+insert into order_item(item_id, count) values(2, 1);
+insert into order_item(item_id, count) values(3, 3);
+insert into order_item(item_id, count) values(4, 9);
--- latheeth/latheeth/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists menu_item 
-(
-    id  serial primary key,
-    name text not null,
-    image text,
-    description text not null
-);
