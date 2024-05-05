3386f8d2e31e822d1f456c7629fd3b9f16dd0e26
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2024-05-05 17:50:18 -0400

    Pass updateOrder as prop to AddItem component and change default for adding item to 0

--- latheeth/latheeth/src/main/client/src/order/AddItem.tsx
+++ latheeth/latheeth/src/main/client/src/order/AddItem.tsx
@@ -1,30 +1,36 @@
 import { useState } from "react";
-import { Order } from "./Orders";
+import { Item } from "../menu/MenuItem";
 
 type Props = {
-    order: Order,
+    item: Item,
+    updateOrder: (count: number) => void,
     onClose: (event: any) => void
 }
 
-function AddItem({ order, onClose }: Props) {
-    const [count, setCount] = useState(order.count);
+function AddItem({ item, updateOrder, onClose }: Props) {
+    const [count, setCount] = useState(0);
+
+    function addItemAndClose(event: React.MouseEvent<HTMLButtonElement>) {
+        updateOrder(count);
+        onClose(event);
+    }
 
     return (
         <div className="AddItem">
-            <h2>{order.item.name}</h2>
-            <img className="add-item-img item-img" src={order.item.image} />
-            <p>{order.item.description}</p>
+            <h2>{item.name}</h2>
+            <img className="add-item-img item-img" src={item.image} />
+            <p>{item.description}</p>
             <div>
                 <label>Count: </label>
                 <input 
-                    min="1" 
+                    min="0" 
                     type="number" 
                     onChange={(e) => setCount(parseInt(e.target.value))} 
-                    defaultValue={order.count} 
+                    defaultValue={0} 
                 />
             </div>
-            <p><span className="total">Total:</span> ${(order.item.unitPrice * count).toFixed(2)}</p>
-            <button className="add-to-order-btn">Add to Order</button>
+            <p><span className="total">Total:</span> ${(item.unitPrice * count).toFixed(2)}</p>
+            <button className="add-btn order-btn" onClick={addItemAndClose}>Update Order</button>
             <button className="cancel-btn" onClick={onClose}>Cancel</button>
         </div>
     );    
